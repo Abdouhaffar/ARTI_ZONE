109b7667c4c00116cf6f0a946a558a3300d08c15
Abdouhaffar <86650365+Abdouhaffar@users.noreply.github.com>
2025-11-15 04:33:55 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,18 +2,21 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "artizone-algeria.firebaseapp.com",
-  projectId: "artizone-algeria",
-  storageBucket: "artizone-algeria.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyDHuYlu4xDoaom-neYGQBYf4iITeybdrvI",
+  authDomain: "artizone-e3d9c.firebaseapp.com",
+  projectId: "artizone-e3d9c",
+  storageBucket: "artizone-e3d9c.firebasestorage.app",
+  messagingSenderId: "370751376477",
+  appId: "1:370751376477:web:804fb26c7f24db5f3c51b4",
+  measurementId: "G-YLTXWE4TC4"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
 export default app;
